bf2f18400b83d54c720ad9915b4410988e5c5c1f
Thelordofdream <574483817@qq.com>
2017-07-07 22:36:58 +0800

    add Google Material Design

--- public/javascripts/add_stars.js
+++ public/javascripts/add_stars.js
@@ -2,23 +2,27 @@
  * Created by zhangmingjie on 2017/6/30.
  */
 function addStar(index){
-    var url = '/data/addstars';
-    var params = [
-        'index='+index.toString()
-    ];
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
-        if (req.readyState=== 4) {
-            var responseHeaders = req.getAllResponseHeaders();
-            var star = req.responseText;
-            star = JSON.parse(star);
-            if(star["msg"]==="点赞成功"){
-                var num = parseInt(document.getElementById(index.toString()).innerText);
-                document.getElementById(index.toString()).innerText = (num + 1).toString();
+    if(check_user()) {
+        var url = '/data/addstars';
+        var params = [
+            'index=' + index.toString()
+        ];
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+            if (req.readyState === 4) {
+                var responseHeaders = req.getAllResponseHeaders();
+                var star = req.responseText;
+                star = JSON.parse(star);
+                if (star["msg"] === "点赞成功") {
+                    var num = parseInt(document.getElementById(index.toString()).innerText);
+                    document.getElementById(index.toString()).innerText = (num + 1).toString();
+                }
             }
-        }
-    };
-    req.open('GET', url + '?' + params.join('&'), true);
-    req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    req.send(null);
+        };
+        req.open('GET', url + '?' + params.join('&'), true);
+        req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        req.send(null);
+    }
+    else
+        alert("请先登录!")
 }
--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -1,10 +1,19 @@
 /**
  * Created by zhangmingjie on 2017/7/7.
  */
+var username = null;
+
 function login(){
     alert("登录");
 }
 
 function register(){
     alert("注册");
+}
+
+function check_user(){
+    if(username === null)
+        return 0;
+    else
+        return 1
 }
