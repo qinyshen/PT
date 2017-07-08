b697a549a4219373f9a08d4744c98f8f2f0fc169
Thelordofdream <574483817@qq.com>
2017-07-08 10:43:39 +0800

    transfer to Google Material Design

--- public/javascripts/list_loading.js
+++ public/javascripts/list_loading.js
@@ -21,12 +21,12 @@ function addpaper(paper, index){
     var newRow=document.createElement("tr");
     var td=document.createElement("td");
     td.innerHTML="<table style='width: 100%;height:auto;margin-bottom: 20px'><tbody>" +
-        "<tr style='height: 80px' bgcolor='white'><td>" + paper['Title'] + "</td></tr>" +
-        "<tr style='height: 40px' bgcolor='white'><td>" + paper['Authors'] + "</td></tr>" +
-        "<tr style='height: 40px' bgcolor='white'><td><table align='left' style='width:50%;height: auto;'><tr><td>" + paper['Date'] + "</td></tr></table><table align=\"right\" style=\"width:50%;height: auto;\"><tr><td><a id=\"arXiv\" target=\“_blank\” href=\"" + paper['Address'] + "\">" + paper['Address'].split("//")[1]  + "</a></td></tr></table></td></tr>" +
-        "<tr style='height: 40px' bgcolor='white'><td>GitHub</td></tr>" +
-        "<tr style='height: 500px' bgcolor='white'><td style='vertical-align:text-top''>" + paper['Abstract'] + "</td></tr>" +
-        "<tr style='height: 40px' bgcolor='white'><td><table align='left' style='width:50%;height: auto;'><tr><td>" + paper['Subject'] + "</td></tr></table><table align='center' style='width:50%;height: auto;'><tr><td id=" + index.toString() + " style='text-align: center'>" + paper['Stars'] + "</td></tr></table></td></tr>" +
+        "<tr style='height: 80px;' bgcolor='white'><td style='padding:10px 20px 0 20px;'>" + paper['Title'] + "</td></tr>" +
+        "<tr style='height: 40px;' bgcolor='white'><td style='padding:0 20px 0 20px;'>" + paper['Authors'] + "</td></tr>" +
+        "<tr style='height: 40px;' bgcolor='white'><td style='padding:0 20px 0 20px;'><table align='left' style='width:50%;height: auto;'><tr><td>" + paper['Date'] + "</td></tr></table><table align=\"right\" style=\"width:50%;height: auto;\"><tr><td><a id=\"arXiv\" target=\“_blank\” href=\"" + paper['Address'] + "\">" + paper['Address'].split("//")[1]  + "</a></td></tr></table></td></tr>" +
+        "<tr style='height: 40px;' bgcolor='white'><td style='padding:0 20px 0 20px;'>GitHub</td></tr>" +
+        "<tr style='height: 500px;' bgcolor='white'><td style='vertical-align:text-top;padding:0 20px 0 20px;'>" + paper['Abstract'] + "</td></tr>" +
+        "<tr style='height: 40px;' bgcolor='white'><td style='padding:0 20px 0 20px;'><table align='left' style='width:50%;height: auto;'><tr><td>" + paper['Subject'] + "</td></tr></table><table align='center' style='width:50%;height: auto;'><tr><td id=" + index.toString() + " style='text-align: center'>" + paper['Stars'] + "</td></tr></table></td></tr>" +
         "<tr style='height: 40px; background-color:rgba(0,0,0,0)'>" +
         "<td><button class='mdl-button mdl-button--raised mdl-button--accent mdl-shadow--4dp' style='background-color: lightskyblue;width:20%;margin-left: 4%' onclick='addStar(" + index + ")'>赞</button>"+
         "<button class='mdl-button mdl-button--raised mdl-button--accent mdl-shadow--4dp' style='background-color: lightskyblue;width:20%;margin-left: 4%'>评论</button>" +
